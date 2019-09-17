bb794ee4e8e2afba6ca8c6477cda386db4f44b1a
Rayven <rayven.burns@gmail.com>
2019-09-17 19:39:00 -0400

    updated

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,7 +35,7 @@ function MovieDetails({ movie }) {
     <div className="movie-card">
   <Link to = {`/movies/${movie.id}`}>
       <h2>{title}</h2>
-      </Link>
+      
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
@@ -49,6 +49,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      </Link>
     </div>
 
   );
